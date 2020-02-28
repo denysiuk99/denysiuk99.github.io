ddd68ca2cdb3cc7de2b52aa29887fcc207098dc3
Vladyslav Denysiuk <denysiuk99@gmail.com>
2020-02-28 21:19:47 +0200

    Added jquery functions.

---
+++ assets/js/app.js
@@ -0,0 +1,53 @@
+$(function () {
+    let introH = $('#intro').innerHeight();
+    let header = $('#header');
+    let scrollOffset = $(window).scrollTop;
+
+    /* Fixed HEADER */
+    checkScroll(scrollOffset);
+
+    $(window).on('scroll', function () {
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+        if (scrollOffset > introH) {
+            header.addClass('fixed');
+        } else {
+            header.removeClass('fixed');
+        }
+    }
+
+
+    /* Scroll to elements */
+
+    $('[data-scroll]').on('click', function (event) {
+        event.preventDefault();
+
+        let $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $('#nav a').removeClass("active");
+        $this.addClass('active');
+
+        $('html, body').animate({
+            scrollTop: blockOffset
+        }, 500)
+    });
+
+
+    /* Burger Menu */
+
+    $('#nav_toggle').on('click', function (event) {
+        event.preventDefault();
+
+        $(this).toggleClass('active');
+
+        $('#nav').toggleClass('active');
+    })
+
+
+});
